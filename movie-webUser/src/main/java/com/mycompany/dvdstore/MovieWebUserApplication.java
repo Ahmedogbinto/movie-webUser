0785c0a6e36357a855404136258bf86065b4148f
AhmedOg <ahmedogbinto@gmail.com>
2022-10-12 16:22:46 -0400

    Update MovieWebUserApplication.java

--- movie-webUser/src/main/java/com/mycompany/dvdstore/MovieWebUserApplication.java
+++ movie-webUser/src/main/java/com/mycompany/dvdstore/MovieWebUserApplication.java
@@ -1,15 +1,10 @@
 package com.mycompany.dvdstore;
 
 import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
-import com.mycompany.dvdstore.core.entity.Movie;
-import com.mycompany.dvdstore.core.service.MovieServiceInterface;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Controller;
 
-import java.util.Scanner;
 
 @SpringBootApplication
 public class MovieWebUserApplication {
